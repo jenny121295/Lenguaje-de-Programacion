f95eb6d72c85c8ff7ddb6d88978435e824f95a4c
jenny121295 <71959603+jenny121295@users.noreply.github.com>
2020-09-28 18:46:53 -0500

    Add files via upload
    Circuito 1: Circuito paralelo

--- main.c
+++ main.c
@@ -1,35 +1,31 @@
-//Programa para calcular la resistencia en un circuito de led
+//Programa para calcular las corrientes y voltajes de cada resistencia en un circuito en paralelo
 //28/09/2020
 //Realizado por Jennifer Guadalupe Valencia Sotelo
 
 #include <stdio.h>
 
-int main ()
-
+int main()
 {
-    float V, V1, V2, V3, I1, I2, I3, R1, R2, R3;
-    V1=2.8;
-    V2=3;
-    V3=1.5;
-    I1=0.02;
-    I2=0.02;
-    I3=0.015;
+    float Vs, I1, I2, I3;
+    int R1, R2, R3;
 
-    printf("Este programa solo calcula la resistencia de los siguientes tipos de leds:\n");
-    printf("1.- Led Blanco.\n");
-    printf("2.- Led Verde brillante.\n");
-    printf("3.- Led Rojo std.\n");
+    printf("Escribe el valor de la fuente:");
+    scanf("%f",&Vs);
+    printf("Resistencia 1:");
+    scanf("%d",&R1);
+    printf("Resistencia 2:");
+    scanf("%d",&R2);
+    printf("Resistencia 3:");
+    scanf("%d",&R3);
 
-    printf("Escribe el valor del voltaje de la fuente:");
-    scanf("%f",&V);
+    printf("Al ser un circuito en paralelo el voltaje de la fuente es el mismo para cada una de las resistencias\n");
+    printf("V1=%.2fv\n",Vs);
+    printf("V2=%.2fv\n",Vs);
+    printf("V3=%.2fv\n",Vs);
 
-    R1=(V-V1)/I1;
-    R2=(V-V2)/I2;
-    R3=(V-V3)/I3;
-
-    printf("La resistencia para el Led Blanco es: %.2f ohms\n",R1);
-    printf("La resistencia para el Led Verde brillante es: %.2f ohms\n",R2);
-    printf("La resistencia para el Led Rojo std es: %.2f ohms\n",R3);
-
+    printf("El valor de las corrientes es:\n");
+    printf("I1=%.2fA\n",Vs/R1);
+    printf("I2=%.2fA\n",Vs/R2);
+    printf("I3=%.2fA\n",Vs/R3);
     return 0;
 }
