a8341dedb874ebb4c482e899c955c577dd6df2f3
Jennifer Guadalupe Valencia Sotelo <71959603+jenny121295@users.noreply.github.com>
2020-10-11 00:46:24 -0500

    Add files via upload

---
+++ 05 SERIE1.c
@@ -0,0 +1,26 @@
+//Programa que resuelve el ejemplo de la primera serie de la formula N^2-2i, desde 0 hasta N
+//11/Octubre/2020
+//Realizado por Jennifer Guadalupe Valencia Sotelo
+
+#include <stdio.h>
+
+int main()
+{
+    int N, i=0,x;
+
+    printf("Este programa da la serie de la formula N^2-2i, desde 0 hasta N.\n");
+    printf("Escribe el valor de N:");
+    scanf("%d",&N);
+
+    printf("Los valores de tu serie son:");
+
+    while(i<=N)
+    {
+        x = N*N-2*i;
+        printf("%d\t",x);
+        i=i+1;
+    }
+    return 0;
+}
+
+
